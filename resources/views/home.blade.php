334ab95dfe9a50f3f4e1cce4000d222966f79e87
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-01 19:30:53 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="p-5 mb-4 bg-light rounded-3">
+<div class="p-5 mb-4 rounded-3">
     <div class="container py-5">
 
         <h1 class="display-5 fw-bold">
