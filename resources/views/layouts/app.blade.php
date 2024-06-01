45ceadf107b82d54146af0603cf8b5541093691a
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-01 19:31:08 +0200

    update sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,149 +24,70 @@
         <div class="container-fluid">
             <div class="row flex-nowrap">
                 <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                    <div
+                        class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                         <a href="{{ url('/') }}"
                             class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                             <span class="fs-5 d-none d-sm-inline">
                                 <i class="bi bi-box"> Boolpress</i>
                             </span>
                         </a>
-                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+                            id="menu">
                             <li>
-                                <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle link-light">
-                                    <i class="fs-4 bi-speedometer2"></i> <span
+                                <a href="#submenu1" data-bs-toggle="collapse"
+                                    class="nav-link px-0 align-middle link-light">
+                                    <i class="fs-4 bi-speedometer2"></i><span
                                         class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-                                <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                <ul class="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                                     <li class="w-100">
-                                        <a href="{{url('admin/projects') }}" class="nav-link px-0 link-light"> <span class="d-none d-sm-inline">{{ __('Admin Projects') }}</span></a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="nav-link px-0 link-light"> <span class="d-none d-sm-inline">Item</span>2</a>
+                                        <a href="{{url('admin/projects') }}" class="nav-link px-0 link-light"> <span
+                                                class="d-none d-sm-inline">{{ __('Admin Projects') }}</span></a>
                                     </li>
                                 </ul>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0 align-middle link-light">
-                                    <i class="fs-4 bi-table"></i> <span
-                                        class="ms-1 d-none d-sm-inline">Orders</span></a>
-                            </li>
-                            <li>
-                                <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle link-light">
-                                    <i class="fs-4 bi-bootstrap"></i> <span
-                                        class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
-                                <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                                    <li class="w-100">
-                                        <a href="#" class="nav-link px-0 link-light"> <span class="d-none d-sm-inline">Item</span>
-                                            1</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="nav-link px-0 link-light"> <span class="d-none d-sm-inline">Item</span>
-                                            2</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle link-light">
-                                    <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span>
-                                </a>
-                                <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                                    <li class="w-100">
-                                        <a href="#" class="nav-link px-0 link-light"> <span
-                                                class="d-none d-sm-inline">Product</span> 1</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="nav-link px-0 link-light"> <span
-                                                class="d-none d-sm-inline">Product</span> 2</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="nav-link px-0 link-light"> <span
-                                                class="d-none d-sm-inline">Product</span> 3</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="nav-link px-0 link-light"> <span
-                                                class="d-none d-sm-inline">Product</span> 4</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0 align-middle link-light">
-                                    <i class="fs-4 bi-people"></i> <span
-                                        class="ms-1 d-none d-sm-inline">Customers</span> </a>
                             </li>
                         </ul>
                         <hr>
-
-
-
-                        <div class="dropdown pb-4">
-                            <a href="#"
-                                class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                                id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30"
-                                class="rounded-circle">
-                               
-                            </a>
-
-                            <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                                <li><a class="dropdown-item" href="#">Profile</a></li>
-                                <li><a class="dropdown-item" href="#">Settings</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
+                        @guest
+                            <ul>
+                                <li class="dropdown pb-4 list-unstyled">
+                                    <a class="d-flex align-items-center text-white text-decoration-none"
+                                        href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                                <li><a class="dropdown-item" href="#">Sign out</a></li>
-                                
+                                @if(Route::has('register'))
+                                    <li class="dropdown pb-4 list-unstyled">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                        @else
+                                <li class="dropdown pb-4 list-unstyled">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <img src="https://cdn.noitatnemucod.net/avatar/100x100/one_piece/user-08.jpeg"
+                                            alt="One Piece - Luffy" width="30" height="30" class="rounded-circle">
+                                        {{ Auth::user()->name }}
+                                    </a>
+                                    <div class=" dropdown-menu dropdown-menu-dark text-small shadow"
+                                        aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ url('admin/dashboard') }}">{{__('Dashboard')}}</a>
+                                        <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                                        <hr class="dropdown-divider">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
                             </ul>
-                            
-                        </div>
-
-
-                        
+                        @endguest
                     </div>
                 </div>
                 <div class="col py-3">
-                    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                        <div class="container">
 
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav ms-auto">
-                                    <!-- Authentication Links -->
-                                    @guest
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                        </li>
-                                        @if (Route::has('register'))
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                            </li>
-                                        @endif
-                                    @else
-                                        <li class="nav-item dropdown">
-                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                {{ Auth::user()->name }}
-                                            </a>
-
-                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                                <a class="dropdown-item"
-                                                    href="{{ url('admin/dashboard') }}">{{__('Dashboard')}}</a>
-                                                <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
-                                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                                    onclick="event.preventDefault();
-                                                                                     document.getElementById('logout-form').submit();">
-                                                    {{ __('Logout') }}
-                                                </a>
-
-                                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                                    class="d-none">
-                                                    @csrf
-                                                </form>
-                                            </div>
-                                        </li>
-                                    @endguest
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
                     <main>
                         @yield('content')
                     </main>
