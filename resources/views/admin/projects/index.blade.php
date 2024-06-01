00595ffef2dba1cfe9f96ef8d75ef83d9ad1dd83
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-01 18:56:09 +0200

    test print table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,17 +4,17 @@
 <div class="container">
     <ul class="navbar-nav list-unstyled">
         @foreach ($projects as $project)
-        <li>
-            <h2>{{ $project->title }}</h2>
-        </li>
-        <li>
-            <p>
-                {{ $project->content }}
-            </p>
-        </li>
-        <li>
-            {{ $project->slug }}
-        </li>
+            <li>
+                <h2>{{ $project->title }}</h2>
+            </li>
+            <li>
+                <p>
+                    {{ $project->content }}
+                </p>
+            </li>
+            <li>
+                {{ $project->slug }}
+            </li>
         @endforeach
     </ul>
 </div>
