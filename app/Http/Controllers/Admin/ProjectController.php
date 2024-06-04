e320b66b7fa0ff5b3c99176ee2f82bcc9c8a24bf
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-04 18:42:31 +0200

    created store and update Request for validation

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage; // Uso questo per gestire i file di upload del client
 // use Illuminate\Support\Facades\DB;
 
 class ProjectController extends Controller
@@ -29,10 +32,14 @@ class ProjectController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProjectRequest $request)
     {
         $form_data = $request->validated();
         $form_data['slug'] = Project::generateSlug($form_data['title']);
+        if($request->hasFile('image')){
+            $path_img = Storage::put('uploads', $request->image);
+        };
+        
         $newPost = Project::create($form_data);
         return redirect()->route('admin.projects.show', $newPost->slug);
     }
@@ -56,7 +63,7 @@ class ProjectController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Project $project)
+    public function update(UpdateProjectRequest $request, Project $project)
     {
         $form_data = $request->all();
         if ($project->title !== $form_data['title']) {
