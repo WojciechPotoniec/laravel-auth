dd98702c4eec06bcbe3c199075e445b1895e7533
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-04 20:14:03 +0200

    added input file

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -12,18 +12,26 @@
             <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
                 value="{{ old('title') }}" minlength="3" maxlength="200" required>
             @error('title')
-                <div class="alert alert-danger">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
             @enderror
             <div id="titleHelp" class="form-text text-white">Inserire minimo 3 caratteri e massimo 200</div>
         </div>
-
+        <div class="mb-3">
+            <img id="uploadPreview" width="100" src="/img/placeholder.png">
+            <label for="image" class="form-label">Image</label>
+            <input type="file" accept="image/*" class="form-control @error('image') is-invalid @enderror"
+                id="uploadImage" name="image" value="{{ old('image') }}" maxlength="255">
+            @error('image')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
         <div class="mb-3">
             <label for="content" class="form-label">Content</label>
             <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" required>
                 {{ old('content') }}
               </textarea>
             @error('content')
-                <div class="alert alert-danger">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -17,22 +17,30 @@
             <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
                 value="{{ old('title', $project->title) }}" minlength="3" maxlength="200" required>
             @error('title')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-            <div id="titleHelp" class="form-text text-white">Inserire minimo 3 caratteri e massimo 200</div>
-        </div>
-        <div class="mb-3">
-            <label for="content" class="form-label">Content</label>
-            <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content"
-                required>{{ old('content', $project->content) }}</textarea>
-            @error('content')
-                <div class="alert alert-danger">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
-        <div class="mb-3">
-            <button type="submit" class="btn btn-danger">Save</button>
-            <button type="reset" class="btn btn-secondary">Reset</button>
-        </div>
+        <div class="d-flex">
+            <div class="media me-4">
+                @if($post->image)
+                    <img class="shadow" width="150" src="{{asset('storage/' . $project->image)}}" alt="{{$project->title}}"
+                        id="uploadPreview">
+                @else
+                    <img class="shadow" width="150" src="/images/placeholder.png" alt="{{$project->title}}" id="uploadPreview">
+                @endif
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">Content</label>
+                <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content"
+                    required>{{ old('content', $project->content) }}</textarea>
+                @error('content')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-danger">Save</button>
+                <button type="reset" class="btn btn-secondary">Reset</button>
+            </div>
     </form>
 </section>
 @endsection
