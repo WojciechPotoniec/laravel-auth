166e0e3cb201dfce1cf0f6af19d89b77531756d5
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-06 22:46:28 +0200

    fix validation

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -23,7 +23,7 @@ class StoreProjectRequest extends FormRequest
     {
         return [
             'title' => 'required|unique:projects|max:200|min:3',
-            'image' => 'nullable|image|max:255',
+            'image' => 'nullable|image|max:10240',
             'content' => 'nullable|max:255',
             'slug' => 'nullable'
         ];
@@ -36,7 +36,7 @@ class StoreProjectRequest extends FormRequest
             'title.unique:projects' => 'Questo titolo esiste già!',
             'title.max' => 'Il titolo deve essere lungo massimo :max caratteri!',
             'title.min' => 'Il titolo deve essere lungo almeno :min caratteri!',
-            'image.max' => 'La URL deve essere lunga massimo :max caratteri!'
+            'image.max' => 'Il peso del file non può essere superiore a 10MB'
         ];
     }
 }
