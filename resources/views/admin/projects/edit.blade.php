cd5bf4f02b207860e2d6bc35ca8c17e7a5cb7289
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-07 15:04:52 +0200

    update edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -13,34 +13,38 @@
         @csrf
         @method('PUT')
         <div class="mb-3">
-            <label for="title" class="form-label">Titolo</label>
+            <label for="title" class="form-label">Titolo:</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
                 value="{{ old('title', $project->title) }}" minlength="3" maxlength="200" required>
             @error('title')
                 <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
-        <div class="d-flex">
-            <div class="media me-4">
-                @if($post->image)
+        <div>
+            <div class="mb-3">Image preview:</div>
+            <div class="media mb-3 me-4">
+                @if($project->image)
                     <img class="shadow" width="150" src="{{asset('storage/' . $project->image)}}" alt="{{$project->title}}"
                         id="uploadPreview">
                 @else
-                    <img class="shadow" width="150" src="/images/placeholder.png" alt="{{$project->title}}" id="uploadPreview">
+                    <img class="shadow" width="150" src="/images/placeholder.png" alt="{{$project->title}}"
+                        id="uploadPreview">
                 @endif
             </div>
-            <div class="mb-3">
-                <label for="content" class="form-label">Content</label>
-                <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content"
-                    required>{{ old('content', $project->content) }}</textarea>
-                @error('content')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-danger">Save</button>
-                <button type="reset" class="btn btn-secondary">Reset</button>
-            </div>
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content:</label>
+            <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content"
+                required>{{ old('content', $project->content) }}</textarea>
+            @error('content')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <button type="submit" class="btn btn-danger">Save</button>
+            <button type="reset" class="btn btn-secondary">Reset</button>
+        </div>
     </form>
 </section>
+@include('partials.editor')
 @endsection
