d1754c1ebb8010d440a75faee17dcc935f92d602
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-07 15:05:38 +0200

    added image to fillable value

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'content', 'slug'];
+    protected $fillable = ['title','image', 'content', 'slug'];
 
     public static function generateSlug($title){
         $slug = Str::slug($title, '-');
