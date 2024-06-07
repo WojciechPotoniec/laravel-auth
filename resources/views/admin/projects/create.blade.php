f3a6a094e0adb1d5938050efcedc3e4e60b71ffe
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-07 15:05:07 +0200

    update create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -17,10 +17,10 @@
             <div id="titleHelp" class="form-text text-white">Inserire minimo 3 caratteri e massimo 200</div>
         </div>
         <div class="mb-3">
-            <img id="uploadPreview" width="100" src="/img/placeholder.png">
             <label for="image" class="form-label">Image</label>
             <input type="file" accept="image/*" class="form-control @error('image') is-invalid @enderror"
-                id="uploadImage" name="image" value="{{ old('image') }}" maxlength="255">
+            id="uploadImage" name="image" value="{{ old('image') }}" maxlength="255">
+            <img id="uploadPreview" width="100" src="/img/placeholder.png">
             @error('image')
                 <div class="invalid-feedback">{{ $message }}</div>
             @enderror
@@ -40,4 +40,5 @@
         </div>
     </form>
 </section>
+@include('partials.editor')
 @endsection
